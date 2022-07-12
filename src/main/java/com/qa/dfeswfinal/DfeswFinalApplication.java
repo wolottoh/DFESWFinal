4d5b0443e3482199cbe125658029511623e7111a
wolottoh <ibuej@yahoo.co.uk>
2022-07-12 09:13:51 +0100

    First commit

---
+++ src/main/java/com/qa/dfeswfinal/DfeswFinalApplication.java
@@ -0,0 +1,13 @@
+package com.qa.dfeswfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfeswFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DfeswFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/dfeswfinal/DfeswFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.dfeswfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DfeswFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
