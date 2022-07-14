13f26df9aa7051ee0cb717e40a659cc911f4e8a9
wolottoh <ibuej@yahoo.co.uk>
2022-07-14 17:35:47 +0100

    Added Swagger depency to pomp file

--- src/main/java/com/qa/dfeswfinal/DfeswFinalApplication.java
+++ src/main/java/com/qa/dfeswfinal/DfeswFinalApplication.java
@@ -2,7 +2,8 @@ package com.qa.dfeswfinal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+@EnableWebMvc
 @SpringBootApplication
 public class DfeswFinalApplication {
 
